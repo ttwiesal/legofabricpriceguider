d60db62da78a2eeacafb3778ed52cde56cfcb847
Alexander Wiesinger <alexander.wiesinger@grollmus.de>
2023-11-24 23:19:56 +0100

    use chrome useragent

--- bricklink/bricklink-price-loader.js
+++ bricklink/bricklink-price-loader.js
@@ -6,12 +6,12 @@ const loadAveragePrice = async (itemId, colorId) => {
     const price = await axios
       .get(`https://www.bricklink.com/catalogPG.asp?P=${itemId}&ColorId=${colorId}`, {
         headers: {
-          'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         },
       })
       .then(({ data }) => {
         const $ = cheerio.load(data);
-        const specificSpan = $('table table:nth-child(5) tr:nth-child(3) td:nth-child(1) table tr:nth-child(4) b'); // tr:nth-child(4) td:nth-child(2) b');
+        const specificSpan = $('table table:nth-child(5) tr:nth-child(3) td:nth-child(1) table tr:nth-child(4) b');
         return specificSpan.html().replace('EUR&nbsp;', '');
       });
 
