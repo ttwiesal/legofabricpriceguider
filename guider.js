2e7e45a999a2e9dbed031ab6f3a28636822ccf3a
Alexander Wiesinger <alexander.wiesinger@grollmus.de>
2023-11-24 23:20:53 +0100

    guide for partlist

--- guider.js
+++ guider.js
@@ -1,4 +1,4 @@
-const guideForPart = async (itemId, colorId) => {
+const guideForPartWithColorId = async (itemId, colorId) => {
   const legoFabricPrice = 9.9;
   const { loadPrice } = require('./bricklink/bricklink-price-loader.js');
   const pricePerPiece = await loadPrice({ itemId, color: colorId });
@@ -23,4 +23,46 @@ const guideForPart = async (itemId, colorId) => {
   }
 };
 
-module.exports = { guideForPart };
+const guideForPartWithColorName = async (itemId, color) => {
+  const { convertToBricklinkId } = require('./rebrickable/colorname-to-id-converter.js');
+
+  const colorId = await convertToBricklinkId({ color });
+
+  if (isNaN(colorId)) {
+    console.log('Color not found!');
+    return;
+  }
+
+  return guideForPartWithColorId(itemId, colorId);
+};
+
+const guideForPartlist = async (username, password) => {
+  const inquirer = require('inquirer');
+  const prompt = inquirer.createPromptModule();
+
+  const { generateToken } = require('./rebrickable/user-token-generator.js');
+  const usertoken = await generateToken({ username, password });
+
+  const { getPartLists, getBricklinkParts } = require('./rebrickable/part-list.js');
+  const partLists = await getPartLists({ usertoken });
+
+  const partlistchoices = partLists.map((partlist) => ({ name: partlist.name, value: partlist.id }));
+
+  const { partlistid } = await prompt([
+    {
+      type: 'list',
+      name: 'partlistid',
+      message: 'Select a partlist',
+      choices: partlistchoices,
+    },
+  ]);
+
+  const partlist = await getBricklinkParts({ usertoken, partlistid });
+
+  partlist.forEach(async ({ itemId, colorId }) => {
+    await guideForPartWithColorId(itemId, colorId);
+    setTimeout(() => {}, 1000);
+  });
+};
+
+module.exports = { guideForPartWithColorName, guideForPartlist };
