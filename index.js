603c593eb3caf0d06aa7681f62d938ab34a28dd1
Alexander Wiesinger <alexander.wiesinger@grollmus.de>
2023-11-24 23:22:10 +0100

    add single and multi mode

--- index.js
+++ index.js
@@ -5,29 +5,26 @@
   global.cliArguments = cliArguments;
 
   if (cliArguments) {
-    const { generateToken } = require('./rebrickable/user-token-generator.js');
-    const usertoken = await generateToken({ username: 'pewkachu', password: '' });
+    const inquirer = require('inquirer');
+    const prompt = inquirer.createPromptModule();
 
-    const { getPartLists, getParts } = require('./rebrickable/part-list.js');
-    const partLists = await getPartLists({ usertoken });
-
-    const partlist = await getParts({ usertoken, partlistid: 556307 });
-    console.log(partlist[0].part);
-    console.log(partlist[0].color);
-
-    // const itemId = cliArguments.itemId;
-    // const color = cliArguments.color;
-
-    // const { convertToBricklinkId } = require('./rebrickable/colorname-to-id-converter.js');
-
-    // const colorId = await convertToBricklinkId({ color });
-
-    // if (isNaN(colorId)) {
-    //   console.log('Color not found!');
-    //   return;
-    // }
-
-    // const { guideForPart } = require('./guider.js');
-    // await guideForPart(itemId, colorId);
+    prompt([
+      {
+        type: 'list',
+        name: 'mode',
+        message: 'Select a mode',
+        choices: [
+          { name: 'Single part', value: 'guide-single' },
+          { name: 'Rebrickable Partlist', value: 'guide-partlist' },
+        ],
+      },
+    ]).then(async ({ mode }) => {
+      const { guideForPartWithColorName, guideForPartlist } = require('./guider.js');
+      if (mode === 'guide-single') {
+        await guideForPartWithColorName(cliArguments.itemId, cliArguments.color);
+      } else if (mode === 'guide-partlist') {
+        await guideForPartlist('username', 'password');
+      }
+    });
   }
 })();
