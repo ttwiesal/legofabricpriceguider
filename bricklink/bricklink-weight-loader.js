09afcae68beba6b22301f914d026004e5f70fd8d
Alexander Wiesinger <alexander.wiesinger@grollmus.de>
2023-11-24 23:21:02 +0100

    use chrome user agent

--- bricklink/bricklink-weight-loader.js
+++ bricklink/bricklink-weight-loader.js
@@ -6,7 +6,7 @@ const loadBricklinkWeight = async (itemId) => {
     const weight = await axios
       .get(`https://www.bricklink.com/v2/catalog/catalogitem.page?P=${itemId}`, {
         headers: {
-          'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         },
       })
       .then(({ data }) => {
